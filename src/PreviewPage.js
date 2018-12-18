986c5a9d35eba29e341bdd252746faa5eee4bf30
Myles Timothy <myles_timothy@Outlook.com>
2018-12-17 21:04:06 -0700

    tv-demo-front-end part 7 phase 3 image working

--- src/PreviewPage.js
+++ src/PreviewPage.js
@@ -11,9 +11,6 @@ class PreviewPage extends Component {
     }
 
     state = {
-        show: '',
-        rating: '',
-        url: '',
         ratingSelected: '',
         urlSelected: '',
         showSelected: ''
@@ -57,7 +54,9 @@ class PreviewPage extends Component {
                             <span>Show Name: {this.state.showSelected}</span>
                             <span>Rating: {this.state.ratingSelected}</span>
                         </h2>
+                        {/* <img src={this.props.url} alt='Show Preview' /> */}
                         <img src={this.state.urlSelected} alt='Show Preview' />
+                        {/* <img src="https://www.google.com/imgres?imgurl=http%3A%2F%2Fimages2.fanpop.com%2Fimage%2Fphotos%2F10900000%2FKing-Of-The-Hill-Title-king-of-the-hill-10923551-720-480.jpg&imgrefurl=http%3A%2F%2Fwww.fanpop.com%2Fclubs%2Fking-of-the-hill%2Fimages%2F10923551%2Ftitle%2Fking-hill-title-screencap&docid=a1Zl9IMbVfnZQM&tbnid=Pe10zHjQ9sLGMM%3A&vet=10ahUKEwiBvqPhvKjfAhW5JDQIHQtTAWUQMwg4KAAwAA..i&w=720&h=480&bih=612&biw=1280&q=king%20of%20the%20hill%20title&ved=0ahUKEwiBvqPhvKjfAhW5JDQIHQtTAWUQMwg4KAAwAA&iact=mrc&uact=8" alt='Show Preview' /> */}
                     </div>
                 </main>
             </div>
