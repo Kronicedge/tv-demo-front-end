fd21c4f50a467adc19c3b3044cfca65abbbc1170
Myles Timothy <myles_timothy@Outlook.com>
2019-01-24 20:49:49 -0700

    REACT: TV Demo (Part 13)

--- src/ManagePage.js
+++ src/ManagePage.js
@@ -14,25 +14,31 @@ class ManagePage extends Component {
         console.log(this.state)
     }
 
-    componentDidMount = () => {
-        fetch('http://localhost:4000/shows/', {
-            method: 'GET',
-            headers: {
-                'content-type': 'applicaton/json'
-            }
-        })
-            .then(res => res.json())
-            .then(tvShows => this.setState({ tvShows }, () => {
+    componentDidMount = async () => {
+        try {
+            const res = await fetch('http://localhost:4000/shows/', {
+                method: 'GET',
+                headers: {
+                    'content-type': 'applicaton/json'
+                }
+            })
+            const tvShows = await res.json()
+
+            this.setState({ tvShows }, () => {
                 console.log(this.state.tvShows)
-            }))
+            })
+        } catch (err) {
+
+        }
+
     }
-    tvShowSelected = () => {
-        this.setState({
-            nameInProgress: this.state.tvShow.name,
-            ratingInProgress: this.props.tvShow.rating,
-            urlInProgress: this.props.tvShow.url
-        })
-    }
+    // tvShowSelected = () => {
+    //     this.setState({
+    //         nameInProgress: this.state.tvShow.name,
+    //         ratingInProgress: this.props.tvShow.rating,
+    //         urlInProgress: this.props.tvShow.url
+    //     })
+    // }
 
     tvShowDeleted = () => {
         this.setState({
@@ -55,83 +61,84 @@ class ManagePage extends Component {
         this.setState({ urlInProgress: e.target.value })
     }
 
-    saveTVShow = (e) => {
-        e.preventDefault()
-        const tvShow = {
-            show: this.state.nameInProgress,
-            rating: this.state.ratingInProgress,
-            url: this.state.urlInProgress
-        }
-
-        fetch('http://localhost:4000/shows/', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(tvShow)
-        }).then(res => res.json())
-            .then(tvShow => console.log(tvShow))
-            .catch(err => console.log(err))
-
-        this.componentDidMount()
-
-
-        this.setState({
-            nameInProgress: '',
-            ratingInProgress: '',
-            urlInProgress: ''
-        })
-    }
-
-    renderShows = () => {
-        if (this.state.tvShows) {
-            return this.state.tvShows.map((tvShow) => {
-                return (<li><TVShow name={tvShow.name} allowDelete={true} selectHandler={this.tvShowSelected} deleteHandler={this.tvShowDeleted}></TVShow></li>)
+    saveTVShow = async (e) => {
+        try {
+            e.preventDefault()
+            const tvShow = {
+                show: this.state.nameInProgress,
+                rating: this.state.ratingInProgress,
+                url: this.state.urlInProgress
+            }
+            const res = await fetch('http://localhost:4000/shows/', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(tvShow)
+            })
+            const savedTVShow = await res.json()
+            console.log(savedTVShow)
+        } catch (err) {
+            console.log(err)
+        } finally {
+            this.componentDidMount()
+            this.setState({
+                nameInProgress: '',
+                ratingInProgress: '',
+                urlInProgress: ''
             })
         }
-        else
-            return null
     }
 
-    render = () => {
-        return (
-            <div onClick={this.consoleLog}>
-                <header className='menu'>
-                    <h1>
-                        <SiteNav />
-                    </h1>
-                </header>
-                <main className='wrapper'>
-                    <div className='sidebar'>
-                        <h2>Shows</h2>
-                        <ul>
-                            {this.renderShows()}
-                        </ul>
-                    </div>
-                    <div className='form'>
-                        <form>
-                            <h2>Add a Show</h2>
-                            <div>
-                                <label onClick={this.tvShows}>Name: </label >
-                                <input type='search' name='Name' onChange={this.changeName} value={this.state.nameInProgress} />
-                            </div>
-                            <div>
-                                <label>Rating:</label>
-                                <input type='search' name='Rating' onChange={this.changeRating} value={this.state.ratingInProgress} />
-                            </div>
-                            <div>
-                                <label>Image URL:</label>
-                                <input type='search' name='Image URL' onChange={this.changeURL} value={this.state.urlInProgress} />
-                            </div>
-                            <button type='submit' onClick={this.saveTVShow}>Create/Update</button>
-                        </form>
-
-                    </div>
-
-                </main>
-            </div>
-        )
+renderShows = () => {
+    if (this.state.tvShows) {
+        return this.state.tvShows.map((tvShow) => {
+            return (<li><TVShow name={tvShow.name} allowDelete={true} selectHandler={this.tvShowSelected} deleteHandler={this.tvShowDeleted}></TVShow></li>)
+        })
     }
+    else
+        return null
+}
+
+render = () => {
+    return (
+        <div onClick={this.consoleLog}>
+            <header className='menu'>
+                <h1>
+                    <SiteNav />
+                </h1>
+            </header>
+            <main className='wrapper'>
+                <div className='sidebar'>
+                    <h2>Shows</h2>
+                    <ul>
+                        {this.renderShows()}
+                    </ul>
+                </div>
+                <div className='form'>
+                    <form>
+                        <h2>Add a Show</h2>
+                        <div>
+                            <label onClick={this.tvShows}>Name: </label >
+                            <input type='search' name='Name' onChange={this.changeName} value={this.state.nameInProgress} />
+                        </div>
+                        <div>
+                            <label>Rating:</label>
+                            <input type='search' name='Rating' onChange={this.changeRating} value={this.state.ratingInProgress} />
+                        </div>
+                        <div>
+                            <label>Image URL:</label>
+                            <input type='search' name='Image URL' onChange={this.changeURL} value={this.state.urlInProgress} />
+                        </div>
+                        <button type='submit' onClick={this.saveTVShow}>Create/Update</button>
+                    </form>
+
+                </div>
+
+            </main>
+        </div>
+    )
+}
 }
 
 export default ManagePage
